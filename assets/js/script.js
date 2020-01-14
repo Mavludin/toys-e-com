08157098e14e447c0f561bc9f08acaeecdb15457
Mavludin <imevlud@gmail.com>
2020-01-14 17:38:40 +0300

    making footer and header responsive

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		autoplay: false,
+		loop: true,
+		nav: false,
+		responsive: {
+			0: {
+				nav: false
+			},
+
+			900: {
+				nav: false
+			}
+		}
+	});
+
+});
