8fa70f3ee40951ce9ded7e01f2a5d7be27104cc9
Mavludin <imevlud@gmail.com>
2020-01-15 17:49:06 +0300

    got to the main sector

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,9 +17,41 @@ $(document).ready(function() {
 	});
 
 
-	$("#footer-content h4").on('click', function(){
-		$(this).find('i').toggleClass('rotate');
-        $(this).next("ul").slideToggle(200)
+	$("#footer-content h4 i").on('click', function(){
+		$(this).toggleClass('rotate');
+        $(this).parent().next("ul").slideToggle(200)
 	});
+
+	if ($(window).width() <= 480 ) {
+    	$("input[type='search']").attr("placeholder","Поиск по сайту");
+	} else $("input[type='search']").attr("placeholder","Поиск по товарам и брендам");
+
+    $(window).resize(function () {
+        if ($(window).width() <= 480) {
+    		$("input[type='search']").attr("placeholder","Поиск по сайту");
+        } else $("input[type='search']").attr("placeholder","Поиск по товарам и брендам");
+    });
+
+ 	$('#hamb-block img').on('click', function() {
+ 		$(this).parent().hide();
+ 		$('#close-menu img').parent().fadeIn();
+ 		$('#mobile-menu').slideToggle(400);
+ 	});
+
+ 	$('#close-menu img').on('click', function() {
+ 		$(this).parent().hide();
+ 		$('#hamb-block img').parent().fadeIn();
+ 		 $('#mobile-menu').slideToggle(400);
+ 	});
+
+ 	$('#show-catalog-menu').on('click', function() {
+ 		$(this).parent().hide();
+ 		$('#catalog-menu').fadeIn();
+ 	});
+
+ 	$('#catalog-menu .back').on('click', function() {
+ 		$(this).parent().hide();
+ 		$('#main-mobile-menu').fadeIn();
+ 	});
 
 });
