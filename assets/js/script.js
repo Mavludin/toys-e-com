038031990ed64bbb404e98f7595730c85dc5b662
Mavludin <imevlud@gmail.com>
2020-01-20 14:52:44 +0300

    done the main page

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,9 +19,7 @@ $(document).ready(function() {
 
 
 	$("#footer-content h4").on('click', function(){
-
 		if ($(this).find('img').css('display') == 'inline-block') {
-
 			$(this).find('img').toggleClass('rotate');
 			$(this).next("ul").slideToggle(200)
 		}
@@ -40,25 +38,34 @@ $(document).ready(function() {
  	$('#hamb-block img').on('click', function() {
  		$(this).parent().hide();
  		$('#close-menu img').parent().fadeIn();
- 		$('#mobile-menu').slideToggle(400);
+ 		$('#mobile-menu.my-dropdown-menu').slideToggle(400);
  	});
 
  	$('#close-menu img').on('click', function() {
  		$(this).parent().hide();
  		$('#hamb-block img').parent().fadeIn();
- 		 $('#mobile-menu').slideToggle(400);
+ 		$('#mobile-menu.my-dropdown-menu').slideToggle(400);
  	});
 
  	$('#show-catalog-menu').on('click', function() {
  		$(this).parent().hide();
- 		$('#catalog-menu').fadeIn();
+ 		$('#mobile-catalog-menu').fadeIn();
  	});
 
- 	$('#catalog-menu .back').on('click', function() {
+ 	$('#mobile-catalog-menu .back').on('click', function() {
  		$(this).parent().hide();
  		$('#main-mobile-menu').fadeIn();
-	 });
+	});
 
-	 $(".owl-nav").before($(".owl-dots"));
+	$('#catalog-link a').on('click', function(e) {
+		e.preventDefault();
+	});
+
+	$('#catalog-link').on('click', function() {
+		$(this).toggleClass('showed');
+ 		$('#large-catalog-menu.my-dropdown-menu').slideToggle(400);
+ 	});
+
+	$(".owl-nav").before($(".owl-dots"));
 
 });
